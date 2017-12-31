296e99352fb4c7970b3b4c40c7a2b2b3bc5b2fde
Abdullah Ilgaz <mail@abdullahilgaz.com>
2018-01-01 01:32:38 +0300

    Project added

---
+++ BasitStringKullanimi.dpr
@@ -0,0 +1,41 @@
+﻿program BasitStringKullanimi;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+    s:string;
+    s1,s2,s3:string;
+
+    ss:shortstring;
+    c:char;
+
+    uzunluk:integer;
+
+begin
+
+  s := 'Uzun bir yolculuk ';
+  s1 := 'tek bir adımla başlar';
+
+  s3 := '42';
+
+  s2 := s + s1;
+
+  ss := 'Örnek cümle';
+
+  uzunluk := Length(s2);
+
+  s3 := Copy(s,6,3);
+
+  c := s[1];
+
+  WriteLn(s3);
+
+  ReadLn;
+
+
+end.
